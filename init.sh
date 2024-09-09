4f4b257c5f8e690c80535c3fd359280236419005
Silvio de Morais <Silvio.LimaDeMorais@va.gov>
2024-09-09 13:05:52 -0400

    Deletion confirmation

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 ./stop.sh
-docker volume rm oracle-data
-docker volume create --name=oracle-data
+
+# Ask for user confirmation
+read -p "Are you sure you want to delete the volume 'oracle-data'? This action cannot be undone. (y/n): " confirm
+
+if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+    docker volume rm oracle-data
+    echo "Volume 'oracle-data' has been deleted."
+
+    docker volume create --name=oracle-data
+    echo "New volume 'oracle-data' has been created."
+else
+    echo "Operation cancelled. The volume was not deleted."
+fi
